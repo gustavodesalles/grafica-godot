c8abb08cb2af272d1d3c0df0235a53b494646708
Gustavo Fukushima <gustavo.fukushima@sc.senai.br>
2024-06-28 21:26:22 -0300

    moving

--- character.gd
+++ character.gd
@@ -7,16 +7,29 @@ const MOUSE_ROTATION = 0.1
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+var is_gingando = false
 
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
+	
+	if is_on_floor():
+		$"robot-early-7"/AnimationTree.set("parameters/do_jump/transition_request", "false")
+
+	if Input.is_action_just_pressed("gingado") and is_on_floor():
+		if is_gingando:
+			$"robot-early-7/AnimationTree".set("parameters/Blend3/blend_amount", 0.0)
+			is_gingando = false
+		else:
+			$"robot-early-7/AnimationTree".set("parameters/Blend3/blend_amount", -1.0)
+			is_gingando = true
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor() and !is_gingando:
 		velocity.y = JUMP_VELOCITY
+		$"robot-early-7"/AnimationTree.set("parameters/do_jump/transition_request", "true")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -25,12 +38,24 @@ func _physics_process(delta):
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
-	else:
+	elif !is_gingando:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
-	move_and_slide()
+	if !is_gingando:
+		move_and_slide()
+		print("X: " + str(input_dir.x) + ", Z: " + str(input_dir.y))
+		if velocity.x != 0 or velocity.z != 0:
+			$"robot-early-7/AnimationTree".set("parameters/Blend3/blend_amount", 1.0)
+			if input_dir.x > 0:
+				$"robot-early-7".rotate_y(deg_to_rad(-3))
+			elif input_dir.x < 0:
+				$"robot-early-7".rotate_y(deg_to_rad(3))
+		else:
+			$"robot-early-7/AnimationTree".set("parameters/Blend3/blend_amount", 0.0)
 
 func _unhandled_input(event):
 	if event is InputEventMouseMotion:
+		#$Camera3D.rotate_y(-lerp(0.0, MOUSE_ROTATION, event.relative.x / 10))
 		rotate_y(-lerp(0.0, MOUSE_ROTATION, event.relative.x / 10))
+		pass
